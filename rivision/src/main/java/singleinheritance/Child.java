57cd6ead666fd6ee269764889ed159e41ae12989
NaSeEb <NaSeEb@DESKTOP-QTFR6NJ>
2024-11-02 10:22:36 -0700

    revision

---
+++ rivision/src/main/java/singleinheritance/Child.java
@@ -0,0 +1,11 @@
+package singleinheritance;
+
+public class Child extends Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child s=new Child();
+		s.add();
+	}
+
+}
---
+++ rivision/src/main/java/singleinheritance/Parent.java
@@ -0,0 +1,11 @@
+package singleinheritance;
+
+public class Parent {
+int a=10,b=20,c;
+public void add()
+{
+	c=a+b;
+	System.out.println(c);
+}
+
+}
